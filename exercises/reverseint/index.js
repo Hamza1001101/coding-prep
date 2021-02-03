8f3f42b88d928fbb45728c6caac36f6363b6432b
Hamza Hayd <dado.org@hotmail.com>
2021-02-03 19:35:27 +0100

    ReverseInt solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,9 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+/**
+ * * Stefans Solution
+ * @param {*} n 
+ */
 function reverseInt(n) {
   const reversed = n.toString().split("").reverse().join("");
-
   return parseInt(reversed) * Math.sign(n);
 }
 
