a4dd1fbd6d31a7f4c6a1235bc509fa46af2a6053
Hamza Hayd <dado.org@hotmail.com>
2021-02-03 18:10:05 +0100

    first commit

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/**
+ * * More manuel approuch
+ * @param {*} str
+ */
+// function reverse(str) {
+//     let reversed = ''
+//     for (const iterator of str) {
+//         reversed = iterator + reversed
+//     }
+//     return reversed
+// }
+
+/**
+ * * Another complicated way of implementing it.
+ * * a bit of more complicated
+ * @param {*} str
+ */
+
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '')
+}
+
+/**
+ * * Easier way of doing
+ * @param {*} str
+ */
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
 
 module.exports = reverse;
