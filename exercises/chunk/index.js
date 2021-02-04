75c23fc63c700ede44d82fd98031f8e3973cc6b2
Hamza Hayd <dado.org@hotmail.com>
2021-02-04 18:16:02 +0100

    chunked solution 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,15 +9,19 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 
+
+
 const chunk = (array, size) => {
   const chunked = [];
   for (let num of array) {
     const last = chunked[chunked.length - 1];
-
     if (!last || last.length === size) {
       chunked.push([num]);
-    } else last.push(num);
+    } else {
+      last.push(num);
+    }
   }
+  return chunked;
 };
 
 module.exports = chunk;
