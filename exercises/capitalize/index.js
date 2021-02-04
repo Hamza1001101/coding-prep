0c550f22abc9ac8eb138774979299efb2c346f16
Hamza Hayd <dado.org@hotmail.com>
2021-02-04 20:21:51 +0100

    capitalize solution 2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -20,4 +20,21 @@ function capitalize(str) {
   return capArr.join(" ");
 }
 
+
+/**
+ * * A complicated way of doing it
+ * * This is not a good way of doing it
+ */
+
+function capitalize(str) {
+    let result = str[0].toUpperCase()
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result+=str[i].toUpperCase()
+        }
+        else result+=str[i]
+    }
+    return result
+ }
+
 module.exports = capitalize;
