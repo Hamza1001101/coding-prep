aac70c983f11ed502e1e9ac63e2829536a57b3fd
Hamza Hayd <dado.org@hotmail.com>
2021-02-04 09:28:01 +0100

    maxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,26 +6,46 @@
 // maxChar("apple 1231111") === "1"
 
 /**
- * * This particular problem can be applied to many 
+ * * This particular problem can be applied to many
  * * other problems
- * @param {*} str 
+ * @param {*} str
  */
 function maxChar(str) {
-    const charMap = {};
-    let max = 0;
-    let maxChar = ''
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
   for (const char of str) {
+    /**
+     * * Nice and neat of doing it. 
+     * * boolean logic.
+     * 
+     * * we using the OR-operator to see if that value exists yet. 
+     * * So one way of handling this eloquently in this particular 
+     * * case would be to say if adding one onto this number 
+     * * resulted in a null value that's fine.
+     * * Then instead assign the value of 1 like so.
+      ** So if this is falsie then otherwise assign one to char 
+      ** at char and we end up with the same objecT       
+     */
     charMap[char] = charMap[char] + 1 || 1;
+
+    /**
+     * Another logic would be
+     */
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
   }
 
-for (let key in charMap) {
-    if (charMap[key]> max) {
-        max = charMap[key]
-        maxChar = key
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
     }
-    return maxChar
-}
-  
+    return maxChar;
+  }
 }
 
 module.exports = maxChar;
