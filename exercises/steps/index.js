2835d91c5964f0a2c4a8feb3fe576bd2393118c7
Hamza Hayd <dado.org@hotmail.com>
2021-02-19 09:06:17 +0100

    steps recursive solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -19,30 +19,45 @@
 
 
 /**
- * First solution
- * @param {n} n 
- */
-/*function steps(n) {
-  
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += "#";
-      } else {
-        stair += " ";
-      }
-    }
-    console.log(stair);
-  }
-}*/
-
-/**
  * * Recursion
  * @param {*} n 
  */
 
-function steps(n) {
+// function steps(n, row = 0, stair = "") {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+//   if (stair.length <= row) {
+//     stair += "#";
+//   } else {
+//     stair += "";
+//   }
+//   steps(n, row, stair);
+// }
+
+const steps = (n) => {
+
+  for (let row = 0; row < n; row++) {
+
+    let hashtag = ''
+    for (let column = 0; column < n; column++) {
+
+      if (column <= row) {
+        hashtag += '#'
+      } 
+      else {
+        hashtag += ' '
+      }
+    }
+
+    console.log(hashtag)
+  }
 }
+
 module.exports = steps;
