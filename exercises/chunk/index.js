11b0d9c64cac263c6b38b05df5e8dd9eaae34005
Hamza Hayd <dado.org@hotmail.com>
2021-04-30 13:17:56 +0200

    added some comment with chunk-solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -18,12 +18,14 @@
 const chunk = (array, size) => {
   const chunked = [];
   for (let num of array) {
+    //*This is undefined for the first run
     const last = chunked[chunked.length - 1];
     /**
      * * if last element doesn't exist, or if its length
      * * is equal to chunck size
      */
     if (!last || last.length === size) {
+      //*push a new subarrat (a new chunk)
       chunked.push([num]);
     } else {
       last.push(num);
@@ -32,25 +34,25 @@ const chunk = (array, size) => {
   return chunked;
 };
 
-/**
- * * We using the slice method here. 
- * @param {*} array 
- * @param {*} size 
- */
-function chunk(array, size) {
-  const chunked = [];
+// /**
+//  * * We using the slice method here. 
+//  * @param {*} array 
+//  * @param {*} size 
+//  */
+// function chunk(array, size) {
+//   const chunked = [];
 
-  //* index variable. this is going to be changing over time
-    let index = 0;
+//   //* index variable. this is going to be changing over time
+//     let index = 0;
     
-  while (index < array.length) {
-    chunked.push(array.slice(index, index + size));
+//   while (index < array.length) {
+//     chunked.push(array.slice(index, index + size));
 
-    //* increment by size not by one
-    index += size;
-  }
-  return chunked;
-}
+//     //* increment by size not by one
+//     index += size;
+//   }
+//   return chunked;
+//}
 
 
 module.exports = chunk;
