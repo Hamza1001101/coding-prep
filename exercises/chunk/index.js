18747574f52152dbd3bb56637e9d5b5848b92344
Hamza Hayd <dado.org@hotmail.com>
2021-04-30 13:38:18 +0200

    added some comment with chunk-solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -34,6 +34,16 @@ const chunk = (array, size) => {
   return chunked;
 };
 
+const chunck2 = (arr, size) => {
+  let index = 0;
+  const chunked = [];
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+};
+
 // /**
 //  * * We using the slice method here. 
 //  * @param {*} array 
@@ -55,4 +65,4 @@ const chunk = (array, size) => {
 //}
 
 
-module.exports = chunk;
+module.exports = chunck2;
