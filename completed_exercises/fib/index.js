bd000f028b4b2d8d76f2f30d89f6384e870e51ca
Hamza Hayd <dado.org@hotmail.com>
2021-04-30 21:15:14 +0200

    added the solution of fibonachi

--- completed_exercises/fib/index.js
+++ completed_exercises/fib/index.js
@@ -8,41 +8,39 @@
 // Example:
 //   fib(4) === 3
 
-function memoize(fn) {
-  const cache = {};
-  return function(...args) {
-    if (cache[args]) {
-      return cache[args];
-    }
+// function memoize(fn) {
+//   const cache = {};
+//   return function(...args) {
+//     if (cache[args]) {
+//       return cache[args];
+//     }
 
-    const result = fn.apply(this, args);
-    cache[args] = result;
+//     const result = fn.apply(this, args);
+//     cache[args] = result;
 
-    return result;
-  };
-}
+//     return result;
+//   };
+// }
 
-function slowFib(n) {
-  if (n < 2) {
-    return n;
-  }
 
+const fib = (n) => {
+  if (n < 2) return n;
   return fib(n - 1) + fib(n - 2);
-}
+};
 
-const fib = memoize(slowFib);
 
-module.exports = fib;
+
 
 // function fib(n) {
 //   const result = [0, 1];
-//
+
 //   for (let i = 2; i <= n; i++) {
 //     const a = result[i - 1];
 //     const b = result[i - 2];
-//
+
 //     result.push(a + b);
 //   }
-//
+
 //   return result[n];
 // }
+module.exports = fib;
