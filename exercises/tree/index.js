1ac91253b0420367499ef97d915d3fe6e7083089
Hamza Hayd <dado.org@hotmail.com>
2021-05-06 15:46:58 +0200

    added a traverseBF method

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -38,6 +38,15 @@ class Tree {
         }
 
     }
+    traverseDF(fn) {
+         const arr = [this.root];
+         while (arr.length) {
+             const node = arr.shift();
+           arr.unshift(...node.children);
+
+           fn(node);
+         }
+    }
 }
 
 module.exports = { Tree, Node };
